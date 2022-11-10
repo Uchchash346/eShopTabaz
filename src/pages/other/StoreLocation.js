13e53d010d6d2a42e908e74458802dcdb42e1d97
Uchchash346 <mru416@gmail.com>
2022-11-10 16:42:52 +0600

    Store location page setup successfully

--- src/pages/other/StoreLocation.js
+++ src/pages/other/StoreLocation.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Fragment } from 'react';
+import LocationMap from '../../components/contact/LocationMap';
 import LayoutOne from '../../layouts/LayoutOne';
 
+
+
 const StoreLocation = () => {
+
+    //  23.783536747568714, 90.39246228413516
     return (
         <Fragment>
             <LayoutOne>
-                <div className='container'>
-                                    
+                <div className='container store-location-map'>
+                    <LocationMap latitude="23.7835159475328" longitude="90.3924746708468" />
                 </div>
             </LayoutOne>
         </Fragment>
